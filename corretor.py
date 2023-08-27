704aa4cf89fe076a078b814eafade0ccaaccff7e
Kevirak <kelwinkaran@gmail.com>
2023-08-27 17:32:25 -0300

    Update corretor.py
    Agora vai informar se acertou ou  errou a questão.

--- corretor.py
+++ corretor.py
@@ -9,13 +9,15 @@ def main():
 
     print("\nAgora, insira as alternativas corretas da lista:")
     for i in range(10):
-        string2 = input(f"{i+1}ª questão. Você respondeu [{lista1[i]}], a resposta esta [C]orreta ou [E]rrada? ")
+        string2 = input(f"{i+1}ª questão. Você respondeu [{lista1[i]}], a resposta correta é: ")
         lista2.append(string2)
 
     # Mostrar as duas listas lado a lado
     print("\nListas lado a lado:")
     for str1, str2 in zip(lista1, lista2):
-        print(f"{str1:20s} | {str2}")
-
+        if str1 == str2:
+            print(f"{str1:20s} | {str2} (Você acertou!)")
+        else:
+            print(f"{str1:20s} | {str2} (Você errou!)")
 if __name__ == "__main__":
     main()
