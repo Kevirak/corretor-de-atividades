f90ba76f401e8bfe652740fbb32b8aae7a337c9f
Kevirak <kelwinkaran@gmail.com>
2023-08-27 18:15:14 -0300

    Update corretor.py
    Corrigi o erro que dava quando comparava maiúscula e minúscula (Agora as resposta e alternativas são registradas como maiúsculas).
    
    Além disso, alterei a formatação do arquivo e troquei umas palavras para ficar mais agradável aos olhos.

--- corretor.py
+++ corretor.py
@@ -1,23 +1,31 @@
+'''Script para me ajudar a corrigir listas de exercícios.'''
+
+
 def main():
-    # Solicitar input das duas listas de 10 strings
+    '''Solicitar input das duas listas de 10 strings'''
     lista1 = []
     lista2 = []
 
     for i in range(10):
         string1 = input(f"Digite a {i + 1}ª resposta: ")
+        string1.upper()
         lista1.append(string1)
 
     print("\nAgora, insira as alternativas corretas da lista:")
     for i in range(10):
-        string2 = input(f"{i+1}ª questão. Você respondeu [{lista1[i]}], a resposta correta é: ")
+        string2 = input(
+            f"{i + 1}ª questão. Você respondeu [{lista1[i]}], o correto é: ")
+        string2.upper()
         lista2.append(string2)
 
     # Mostrar as duas listas lado a lado
-    print("\nListas lado a lado:")
+    print("\nLista corrigida:")
     for str1, str2 in zip(lista1, lista2):
         if str1 == str2:
             print(f"{str1:20s} | {str2} (Você acertou!)")
         else:
             print(f"{str1:20s} | {str2} (Você errou!)")
+
+
 if __name__ == "__main__":
     main()
